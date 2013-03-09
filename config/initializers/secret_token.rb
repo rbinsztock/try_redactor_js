af1d32b210abb2bb3f86c4bee42b38dbe4db2b7d
Binsztock Remy <remy@rbweb.fr>
2013-03-09 15:52:00 +0100

    Trying Redactor JS

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redactor::Application.config.secret_key_base = 'c2a9d14079a73e14f1e6dee9ceb578e0e1536cdae9222a09976a460af16beabd4614908f009765328e9c7bdc9a1eba9fcf6c8b83ffe3a8b38693bd3306a23beb'
